6736b038dfde227df275f654f1fe23aa7d4c3e7e
alexpantyukhin <apantykhin@gmail.com>
2016-09-29 20:56:51 +0400

    add SeedListActorSpec

---
+++ Akka.Cluster.Management.Tests/SeedListActorSpec.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Cluster.Management.SeedList;
+
+namespace Akka.Cluster.Management.Tests
+{
+    public class SeedListActorSpec : FSMSpecBase<SeedListState, ISeedListData>
+    {
+
+    }
+}
