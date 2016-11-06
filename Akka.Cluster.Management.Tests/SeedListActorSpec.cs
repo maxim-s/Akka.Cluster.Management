1cc035ab9ca5b9479a16c4bdae3f61652a27aa60
alexpantyukhin <apantykhin@gmail.com>
2016-11-06 23:09:22 +0400

    Add `SeedListActorSpec`

--- Akka.Cluster.Management.Tests/SeedListActorSpec.cs
+++ Akka.Cluster.Management.Tests/SeedListActorSpec.cs
@@ -1,14 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
+using Akka.Actor;
 using Akka.Cluster.Management.SeedList;
+using Akka.Cluster.Management.ServiceDiscovery;
+using Xunit;
 
 namespace Akka.Cluster.Management.Tests
 {
     public class SeedListActorSpec : FSMSpecBase<SeedListState, ISeedListData>
     {
+        private IActorRef Init()
+        {
+            var seedList = Sys.ActorOf(Props.Create(() => new SeedListActor(ServiceDiscoveryClientMock.Object, Settings)));
+            seedList.Tell(new FSMBase.SubscribeTransitionCallBack(StateProbe.Ref));
+            ExpectInitialState(SeedListState.AwaitingInitialState);
+            return seedList;
+        }
 
+        //private Task<GetNodesResponse> FetchSeedsReq()
+        //{
+        //    return ServiceDiscoveryClientMock.Get(Settings.SeedsPath, true, true);
+        //}
+
+        [Fact]
+        private void Seed_list_manager_actor_should_proceed_to_AwaitCommand_when_seed_lists_are_empty()
+        {
+            var seedsTask = new TaskCompletionSource<GetNodesResponse>();
+            ServiceDiscoveryClientMock.Setup(s => s.Get(Settings.SeedsPath, true, true)).Returns(seedsTask.Task);
+
+            var seedList = Init();
+
+            seedList.Tell(new InitialState(ImmutableHashSet<string>.Empty));
+            ExpectTransitionTo(SeedListState.AwaitingRegisteredSeeds);
+
+            seedsTask.SetResult(new GetNodesResponse(new Dictionary<string, string>()));
+            ExpectTransitionTo(SeedListState.AwaitingCommand);
+        }
     }
 }
